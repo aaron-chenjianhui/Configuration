af254e222e152e791101a9942cd19ee81559fce5
unknown <CHENJH43@cn.midea.com>
2018-09-30 13:38:06 +0800

    add Matlab configurations

---
+++ Matlab/StandardFunctionTemplate.m
@@ -0,0 +1,40 @@
+function [output1,output2] = StandardFunctionTemplate(input1,input2,input3)
+%StandardFunctionTemplate inserts a template to document a function
+%
+% You can provide detailed explanations here.
+%
+% SYNTAX
+%     [output1,output2] = StandardFunctionTemplate(input1,input2,input3)
+%        
+% INPUT
+%      input1: the explanation for input1
+%      input2: the explanation for input2
+%      input3: the explanation for input3
+%
+% OUTPUT
+%      output1: the explanation for output1
+%      output2: the explanation for output2
+% 
+% EXAMPLES
+%      [output1,output2] = StandardFunctionTemplate(input1,input2,input3)
+%      
+% M-FILES required: none
+%    
+% MAT-FILES required: none
+%
+%
+% Author:           Jianhui Chen
+% email:            Jianhui.Chen@kuka.com
+% Matlab ver.:      9 (R2016b)
+% Date:             15-Aug-2018
+% Update:           15-Aug-2018 Jianhui Chen
+% Version:          1.0.0 - created
+
+%--------------------------------------------------------------------------
+
+
+
+%% Your code here
+%------------- BEGIN CODE --------------
+disp('Hellow world');
+%------------- END OF CODE --------------
---
+++ Matlab/StandardScriptTemplate.m
@@ -0,0 +1,27 @@
+%--------------------------------------------------------------------------
+% 
+% StandardScriptTemplate.m inserts a template to document a matlab script
+%
+% The first line of the function must be the definition header (no blank
+% lines expected). Currently DocFun does not support multi-line headers.
+%
+% M-FILES required: none
+%
+% MAT-FILES required: none
+%
+%
+% Author:           Jianhui Chen
+% email:            Jianhui.Chen@kuka.com
+% Matlab ver.:      9 (R2016b)
+% Date:             15-Aug-2018
+% Update:           15-Aug-2018 Jianhui Chen
+% Version:          1.0.0 - created
+
+%--------------------------------------------------------------------------
+
+
+
+%% Your code here
+%------------- BEGIN CODE --------------
+disp('Hellow world');
+%------------- END OF CODE --------------
